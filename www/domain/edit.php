a6b904e9c22d9f78fe6ddd46f741215548cfc087
chun <>
2024-03-18 09:46:33 +0800

    新增刪除/編輯資料

--- www/domain/edit.php
+++ www/domain/edit.php
@@ -18,90 +18,40 @@ include find('./component/sidebar.php');
 						<label
 							for="exampleInputINTEGER"
 							class="form-label"
-						>SN</label>
+						>序號</label>
 						<input
 							type="INTEGER"
 							class="form-control"
 							id="exampleInputINTEGER"
 							aria-describedby="INTEGERHelp"
+							placeholder='序號'
 						>
+						</div>
 						<div class="mb-3">
 							<label
 								for="exampleInputName"
 								class="form-label"
-							>Name</label>
+							>領域名稱</label>
 							<input
 								type="TEXT"
 								class="form-control"
 								id="exampleInputName"
+								placeholder='名稱'
 							>
 						</div>
 						<div class="mb-3">
 							<label
 								for="exampleInputIntro"
 								class="form-label"
-							>Intro</label>
+							>簡介</label>
 							<input
 								type="TEXT"
 								class="form-control"
 								id="exampleInputIntro"
+								placeholder='簡介'
 							>
 						</div>
-						<div class="mb-3">
-							<label
-								for="exampleInputWhenCreated"
-								class="form-label"
-							>WhenCreated</label>
-							<input
-								type="DATETIME"
-								class="form-control"
-								id="exampleInputWhenCreated"
-							>
-						</div>
-						<div class="mb-3">
-							<label
-								for="exampleInputWhenLastEdited"
-								class="form-label"
-							>WhenLastEdited</label>
-							<input
-								type="DATETIME"
-								class="form-control"
-								id="exampleInputWhenLastEdited"
-							>
-						</div>
-						<div class="mb-3">
-							<label
-								for="exampleInputParentSN"
-								class="form-label"
-							>ParentSN</label>
-							<input
-								type="INTEGER"
-								class="form-control"
-								id="exampleInputParentSN"
-							>
-						</div>
-						<div class="mb-3">
-							<label
-								for="exampleInputCreatorSN"
-								class="form-label"
-							>CreatorSN</label>
-							<input
-								type="INTEGER"
-								class="form-control"
-								id="exampleInputCreatorSN"
-							>
-						</div>
-						<div class="mb-3 form-check">
-							<input
-								type="checkbox"
-								class="form-check-input"
-								id="exampleCheck1"
-							>
-							<label
-								class="form-check-label"
-								for="exampleCheck1"
-							>Check me out</label>
-						</div>
+						
 						<button
 							type="submit"
 							class="btn btn-primary"
--- www/domain/view.php
+++ www/domain/view.php
@@ -5,13 +5,21 @@ $layout = './layout/layout.php';
 require '../arranger.php';
 
 $page = $_GET['page'] ?? 1;
+$table = 'Domain';
+$total = connect()->query("SELECT COUNT(*) FROM {$table}")->fetch()[0];
 $limit = 10;
+$pages = ceil($total / $limit);
 $start = $limit * ($page - 1);
-$columns = ['SN','Name','Intro','WhenCreated','WhenLastEdited','ParentSN','CreatorSN'];
+$columns = [
+	"{$table}.SN" => '序號',
+	'Name' => '名稱',
+	'FirstName' => '建立者'
+];
 $statement = connect()->prepare(
 	sprintf(
-		"SELECT %s FROM Domain LIMIT ?, ?",
-		implode(', ', $columns)
+		"SELECT %s FROM %s JOIN Staff ON {$table}.CreatorSN = Staff.SN LIMIT ?, ?",
+		implode(', ', array_keys($columns)),
+		$table
 	)
 );
 
@@ -21,9 +29,54 @@ include find('./component/sidebar.php');
 ?>
 
 <div class="body-wrapper">
-<?php include find('./component/header.php') ?>
+	<?php include find('./component/header.php') ?>
 
 	<div class="container-fluid">
+		<nav aria-label="...">
+			<ul class="pagination">
+				<li class="page-item ">
+					<a
+						class="page-link <?= $page > 1 ? '' : 'disabled' ?>"
+						href="?page=1"
+					>
+						<i data-feather="chevrons-left"></i>
+					</a>
+				</li>
+				<?php if ($page > 1): ?>
+					<li class="page-item">
+						<a
+							class="page-link"
+							href="?page=<?= $page - 1 ?>"
+						><?= $page - 1 ?></a>
+					</li>
+				<?php endif ?>
+				<li
+					class="page-item active"
+					aria-current="page"
+				>
+					<a
+						class="page-link"
+						href="?page=<?= $page ?>"
+					><?= $page ?></a>
+				</li>
+				<?php if ($page < $pages): ?>
+					<li class="page-item">
+						<a
+							class="page-link"
+							href="?page=<?= $page + 1 ?>"
+						><?= $page + 1 ?></a>
+					</li>
+				<?php endif ?>
+				<li class="page-item">
+					<a
+						class="page-link <?= $page < $pages ? '' : 'disabled' ?>"
+						href="?page=<?= $pages ?>"
+					>
+						<i data-feather="chevrons-right"></i>
+					</a>
+				</li>
+			</ul>
+		</nav>
 		<div class="row">
 			<div class="col-lg-12 d-flex align-items-stretch">
 				<div class="card w-100">
@@ -33,17 +86,23 @@ include find('./component/sidebar.php');
 							<table class="table text-nowrap mb-0 align-middle">
 								<thead class="text-dark fs-4">
 									<tr>
-										<?php foreach ($columns as $column) : ?>
+										<?php foreach (array_values($columns) as $column): ?>
 											<th class="border-bottom-0 fw-semibold mb-0"><?= $column ?></th>
 										<?php endforeach ?>
 									</tr>
 								</thead>
 								<tbody>
-									<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)) : ?>
+									<?php while ($row = $statement->fetch(PDO::FETCH_ASSOC)): ?>
 										<tr>
-											<?php foreach ($row as $column) : ?>
+											<?php foreach ($row as $column): ?>
 												<td class="border-bottom-0 mb-0"><?= $column ?></td>
 											<?php endforeach ?>
+											<td>
+												<a class="btn btn-primary m-1">新增</a>
+												<a class="btn btn-success m-1">編輯</a>
+												<a class="btn btn-outline-danger m-1" href="delete.php?sn=<?= $row['SN'] ?>">刪除</a>
+											</td>
+
 										</tr>
 									<?php endwhile ?>
 								</tbody>
