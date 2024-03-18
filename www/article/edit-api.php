316aa21070113020e1f59d998ba5f9fa88f788a8
jackall <dc951236@gmail.com>
2024-03-19 00:46:30 +0800

    沒有特別改什麼

--- www/article/edit-api.php
+++ www/article/edit-api.php
@@ -1,9 +1,10 @@
 <?php
 
+
 $layout = './layout/layout.php';
 require '../arranger.php';
 
-header('Location: view.php');
+// header('Location: view.php');
 
 $statement = connect()->prepare("UPDATE `Article` SET `Identifier`=?,`Title`=?,`WhenLastEdited`=CURRENT_TIMESTAMP,`AuthorSN`=1 WHERE `SN`=?");
 $statement->execute([$_POST['Identifier'], $_POST['Title'], $_GET['sn']]);
