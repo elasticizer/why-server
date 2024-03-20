1b828d96cd44e57625ff6bc54c159f78dbc8287d
Poyuan <poyuan510214@gmail.com>
2024-03-20 10:54:59 +0800

    加入ParentSN新增欄位

--- www/category/add-api.php
+++ www/category/add-api.php
@@ -6,6 +6,6 @@ header('Location: view.php');
 
 if (!empty($_POST['name'])) {
 	$statement = connect()->prepare("INSERT INTO `Category` (`Name`, `Intro`,
-	`Implicit`,`CreatorSN`) VALUES (?, ?, ?, 1)");
-	$statement->execute([$_POST['name'], $_POST['intro'], $_POST['checkbox'] === 'on' ? '是' : '否']);
+	`Implicit`,`ParentSN`,`CreatorSN`) VALUES (?, ?, ?, ?, 1)");
+	$statement->execute([$_POST['name'], $_POST['intro'], $_POST['checkbox'] === 'on' ? 1 : 0, $_POST['parent'],]);
 }
