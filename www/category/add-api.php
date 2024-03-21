3c091ff4e0fc52a5f5d2023ff5802d68f06c5eba
Poyuan <poyuan510214@gmail.com>
2024-03-21 17:33:47 +0800

    修改CreatorSN->$_SESSION['sn']

--- www/category/add-api.php
+++ www/category/add-api.php
@@ -5,6 +5,6 @@ require '../arranger.php';
 header('Location: index.php');
 
 if (isset($_POST['identifier'], $_POST['name'], $_POST['intro'])) {
-	$statement = connect()->prepare("INSERT INTO `Category` (`Identifier`, `Name`, `Intro`, `Implicit`, `ParentSN`, `CreatorSN`) VALUES (?, ?, ?, ?, ?, -2)");
-	$statement->execute([$_POST['identifier'], $_POST['name'], $_POST['intro'], $_POST['checkbox'] ?? 0, $_POST['parent']]);
+	$statement = connect()->prepare("INSERT INTO `Category` (`Identifier`, `Name`, `Intro`, `Implicit`, `ParentSN`, `CreatorSN`) VALUES (?, ?, ?, ?, ?, ?)");
+	$statement->execute([$_POST['identifier'], $_POST['name'], $_POST['intro'], $_POST['checkbox'] ?? 0, $_POST['parent'], $_SESSION['sn']]);
 }
