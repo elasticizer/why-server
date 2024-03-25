301672a184ce3a34ba1257fd8a6da04551516787
jackall <dc951236@gmail.com>
2024-03-25 12:26:30 +0800

    編輯完成跳回index.php

--- www/article/edit-api.php
+++ www/article/edit-api.php
@@ -4,7 +4,8 @@
 $layout = './layout/layout.php';
 require '../arranger.php';
 
-// header('Location: index.php');
+
+header('Location: index.php');
 
 $statement = connect()->prepare("UPDATE `Article` SET `Identifier`=?,`Title`=?,`Content`=?,`WhenLastEdited`=CURRENT_TIMESTAMP,`AuthorSN`=1 WHERE `SN`=?");
 $statement->execute([$_POST['Identifier'], $_POST['Title'], $_POST['Content'], $_GET['sn']]);
